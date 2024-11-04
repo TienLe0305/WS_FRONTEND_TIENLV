03ed84f4f04da68544e816f8b668e00502df7057
Le Van Tien <lvt030501@gmail.com>
2024-11-05 02:31:02 +0700

    improve: clean code

---
+++ src/components/icons/IconDown.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="dropdown-arrow"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+  >
+    <path
+      fill-rule="evenodd"
+      d="M5.23 7.21a.75.75 0 011.06.02L10 10.94l3.71-3.71a.75.75 0 111.06 1.06l-4.24 4.24a.75.75 0 01-1.06 0L5.25 8.27a.75.75 0 01-.02-1.06z"
+      clip-rule="evenodd"
+    />
+  </svg>
+</template>
